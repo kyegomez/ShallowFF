731e0450d85a079ea96debdc5427e6e942872b7e
Kye <kye@apacmediasolutions.com>
2023-11-20 00:00:20 -0800

    alrblock

--- example.py
+++ example.py
@@ -2,8 +2,101 @@ import torch
 import torch.nn.functional as F
 from einops import rearrange
 from torch import einsum, nn
-import math
-from zeta.nn import FeedForward, Residual
+from zeta.structs.attn_layers import GLU
+from zeta.structs.transformer import ReluSquared
+
+
+def exists(val):
+    return val is not None
+
+
+def default(val, default_val):
+    return default_val if val is None else val
+
+
+def init_zero_(layer):
+    nn.init.constant_(layer.weight, 0.0)
+    if exists(layer.bias):
+        nn.init.constant_(layer.bias, 0.0)
+
+
+class FeedForward(nn.Module):
+    """
+    Feedforward neural network with LayerNorms and GELU activations
+
+    Args:
+        dim (int): Input dimension
+        hidden_dim (int): Hidden dimension
+        dropout (float): Dropout probability
+
+    Usage:
+    >>> model = FeedForward(768, 2048, 0.1)
+    >>> x = torch.randn(1, 768)
+    >>> model(x).shape
+
+    """
+
+    def __init__(
+        self,
+        dim: int,
+        dim_out: int = None,
+        mult=4,
+        glu=False,
+        glu_mult_bias=False,
+        swish=False,
+        relu_squared=False,
+        post_act_ln=False,
+        dropout: float = 0.0,
+        no_bias=False,
+        zero_init_output=False,
+    ):
+        super().__init__()
+        inner_dim = int(dim * mult)
+        dim_out = default(dim_out, dim)
+
+        if relu_squared:
+            activation = ReluSquared()
+        elif swish:
+            activation = nn.SiLU()
+        else:
+            activation = nn.GELU()
+
+        if glu:
+            project_in = GLU(dim, inner_dim, activation, mult_bias=glu_mult_bias)
+        else:
+            project_in = nn.Sequential(
+                nn.Linear(dim, inner_dim, bias=not no_bias), activation
+            )
+
+        if post_act_ln:
+            self.ff = nn.Sequential(
+                project_in,
+                nn.LayerNorm(inner_dim),
+                nn.Dropout(dropout),
+                nn.Linear(inner_dim, dim_out, bias=not no_bias),
+            )
+        else:
+            self.ff = nn.Sequential(
+                project_in,
+                nn.Dropout(dropout),
+                nn.Linear(inner_dim, dim_out, bias=not no_bias),
+            )
+
+        # init last linear layer to 0
+        if zero_init_output:
+            init_zero_(self.ff[-1])
+
+    def forward(self, x):
+        """
+        Forward pass of the feedforward network
+
+        Args:
+            x (torch.Tensor): Input tensor
+
+        Returns:
+            torch.Tensor: Output tensor
+        """
+        return self.ff(x)
 
 
 # helpers
@@ -199,7 +292,7 @@ class ALRBlock(nn.Module):
         self.dropout = dropout
 
         self.ffn = FeedForward(dim, hidden_dim, dropout)
-        self.ff = FeedForward(dim, dim, dropout)
+        self.ff = FeedForward(dim, hidden_dim, dropout)
 
         self.to_q_proj = nn.Linear(dim, dim)
         self.to_k_proj = nn.Linear(dim, dim)
@@ -212,36 +305,43 @@ class ALRBlock(nn.Module):
 
         qkv = torch.cat((q, k, v), dim=-1)
         
-        ffn = self.ffn(qkv) + x
-        norm_ffn = self.norm(ffn)
+        ffn = self.ffn(qkv) 
+        norm_ffn = self.norm(ffn) + x
 
-        ff = self.ff(norm_ffn) + norm_ffn
-
-        return ff
-
-
+        ff = self.ff(norm_ffn) 
+        ff_norm = self.norm(ff)
         
+        out = ff_norm + x
+
+        return out
+
+
+x = torch.randn(1, 1024, 512)
+model = ALRBlock(512, 2048, 0.1)
+out = model(x)
+print(out.shape)
 
 
 
-class ALRTransformer(nn.Module):
-    def __init__(
-        self,
-        dim,
-        depth,
-        num_tokens,
-        dim_head=64,
-        heads=8,
-        ff_mult=4,
-    ):
-        super().__init__()
-        self.emb = nn.Embedding(num_tokens, dim)
 
-        self.transformer = Transformer(dim, depth, heads, dim_head, ff_mult)
+# class ALRTransformer(nn.Module):
+#     def __init__(
+#         self,
+#         dim,
+#         depth,
+#         num_tokens,
+#         dim_head=64,
+#         heads=8,
+#         ff_mult=4,
+#     ):
+#         super().__init__()
+#         self.emb = nn.Embedding(num_tokens, dim)
 
-        self.to_logits = nn.Sequential(RMSNorm(dim), nn.Linear(dim, num_tokens))
+#         self.transformer = Transformer(dim, depth, heads, dim_head, ff_mult)
 
-    def forward(self, x):
-        x = self.emb(x)
-        x = self.transformer(x)
-        return self.to_logits(x)
+#         self.to_logits = nn.Sequential(RMSNorm(dim), nn.Linear(dim, num_tokens))
+
+#     def forward(self, x):
+#         x = self.emb(x)
+#         x = self.transformer(x)
+#         return self.to_logits(x)
